a2299fd1c833e2bcebacc2ac78c83c59adf3e5b0
Garcez <allan.f.garcez@gmail.com>
2019-10-31 23:48:12 -0300

    Add clock object

---
+++ src/clock/index.ts
@@ -0,0 +1,31 @@
+import moment from "moment";
+
+export const BR_DATE_MASK = "DD/MM/YYYY";
+export const ISO_DATE_MASK = "YYYY-MM-DD";
+export const BR_DATETIME_MASK = "DD/MM/YYYY HH:mm";
+export const BR_HOUR_MASK = "HH:mm";
+
+export const today = moment();
+export const isoTodayInit = moment().format(`${ISO_DATE_MASK}T00:00:00`);
+export const isoTodayEnd = moment().format(`${ISO_DATE_MASK}T23:59:59`);
+
+export const toDate = (date?: string, format = "ZZ", masked = BR_DATE_MASK) => {
+	if (date) {
+		return moment(date, format).format(masked);
+	}
+	return moment().format(masked);
+};
+
+export const dateHour = (date?: string, format = "ZZ") => {
+	if (date) {
+		return moment(date, format).format(BR_DATETIME_MASK);
+	}
+	return moment().format(BR_DATETIME_MASK);
+};
+
+export const toHour = (date: string, format = "ZZ") => {
+	if (date) {
+		return moment(date, format).format(BR_HOUR_MASK);
+	}
+	return moment().format(BR_HOUR_MASK);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./format";
 export * from "./format/BrFormat";
-export * from "./utils";
 export { Equals } from "./validations/Equals";
 
