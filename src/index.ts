b8582f3ccccda9d9dab9a2f16a5294ddbf637253
Garcez <allan.f.garcez@gmail.com>
2019-11-16 01:27:45 -0300

    correct exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export { default as HttpClient } from "./http-client";
 export { default as Console } from "./html/console";
 export * from "./format/br-format";
 export * from "./validations";
-export { bbCode as BbCode } from "./html/bbcode";
+export { BbCodeParser } from "./html/bbcode";
 export * from "./format/number";
 export * from "./format/dates";
