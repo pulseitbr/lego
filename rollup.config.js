87191985c67de705cb71542ab1575576acf7aff2
g4rcez <allan.f.garcez@gmail.com>
2019-12-21 14:59:23 -0300

    Update build config and packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,67 @@
+import resolve from "@rollup/plugin-node-resolve";
+import url from "@rollup/plugin-url";
+import svgr from "@svgr/rollup";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
-import resolve from "rollup-plugin-node-resolve";
-import svgr from "@svgr/rollup";
+import strip from "@rollup/plugin-strip";
 import typescript from "rollup-plugin-typescript2";
-import url from "rollup-plugin-url";
+import visualizer from "rollup-plugin-visualizer";
 import pkg from "./package.json";
 
 export default {
 	input: "src/index.ts",
+	cache: true,
 	output: [
 		{
 			file: pkg.main,
 			format: "cjs",
 			exports: "named",
-			sourcemap: true
+			sourcemap: false
 		},
 		{
 			file: pkg.module,
 			format: "es",
 			exports: "named",
-			sourcemap: true
+			sourcemap: false
+		},
+		{
+			file: pkg.module,
+			format: "esm",
+			exports: "named",
+			sourcemap: false
 		}
 	],
+	external: ["react", "styled-components"],
 	plugins: [
+		strip(),
 		external(),
 		postcss({
-			plugins: [],
+			inject: true,
 			extract: true,
-			minimize: true
+			minimize: true,
+			autoModules: true,
+			extensions: [".css"]
 		}),
 		url(),
+		visualizer(),
 		svgr(),
-		resolve(),
+		resolve({
+			browser: true,
+			preferBuiltins: false
+		}),
 		typescript({
 			rollupCommonJSResolveHack: true,
 			clean: true
 		}),
-		commonjs()
+		commonjs({
+			sourceMap: false,
+			ignoreGlobal: false,
+			namedExports: {
+				"react-is": ["isElement", "isValidElementType", "ForwardRef"],
+				react: ["cloneElement", "Fragment", "createContext", "Component", "createElement"]
+			}
+		})
 	]
 };
+
